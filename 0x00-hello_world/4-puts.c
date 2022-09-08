92a37d009002744021cf6e772a8f2afe9971468e
Musa Sylvester Echi <msaintsly@gmail.com>
2022-09-08 16:30:16 -0700

    task 4

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,12 @@
+#include <studio.h>
+
+/**
+ * main - Printing
+ * 
+ * Return: Always 0 (Successful)
+ */
+int main(void)
+{
+	puts("\"Programming is like building a multiligual puzzle");
+	return (0);
+}
