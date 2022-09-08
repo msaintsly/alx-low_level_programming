09301b3a76a351e38a2caa9cfd318d04677eecb8
Musa Sylvester Echi <msaintsly@gmail.com>
2022-09-08 16:57:52 -0700

    coding

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,12 +1,13 @@
 #include <studio.h>
-
 /**
- * main - Printing
- * 
- * Return: Always 0 (Successful)
+ * main - print the string in the put function
+ *
+ * Description: using the main function
+ * this program prints "Programming is like building a multilingual puzzle
+ * Return: 0
  */
 int main(void)
 {
-	puts("\"Programming is like building a multiligual puzzle");
+	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 }
