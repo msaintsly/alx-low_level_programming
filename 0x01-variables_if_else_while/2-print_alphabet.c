a2b122175f198482bc798cef2eb964fcc3d6b89f
Musa Sylvester Echi <msaintsly@gmail.com>
2022-09-09 06:36:55 -0700

    tasdk 2

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - the alphabet game
+ *
+ *Return: Always 0 (success)
+ */
+int main(void)
+{
+	int alphabet;
+	for (alphabet = 'a'; alphabet <= 'z'; alphabet++);
+	putcher(alphabet);
+	putchar('\n');
+	return (0);
+}
+		
+	
