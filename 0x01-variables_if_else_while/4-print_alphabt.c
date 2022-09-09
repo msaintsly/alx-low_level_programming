edeebd0bdb9f44987ccae2ffcc73c22d092e87ff
Musa Sylvester Echi <msaintsly@gmail.com>
2022-09-09 13:07:55 -0700

    task 4

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ * main - point of entry
+ *
+ * Return: Always 0 (Successful)
+ */
+
+int main(void)
+{
+	char lower_case, e, q;
+
+	e = 'e';
+	q = 'q';
+
+	for (lower_case = 'a';lower_case <= 'z'; lower_case++)
+		if (low != e && low !=q)
+			putchar (lower_case)
+}
+putchar('\n');
+
+return (0;)
+
