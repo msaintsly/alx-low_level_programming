a13a55a8586c6cb2cd0d03b58829e08bb1d8b62e
Musa Sylvester Echi <msaintsly@gmail.com>
2022-09-09 05:29:38 -0700

    task 0

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -16,16 +16,16 @@ int main(void)
 	n = rand() - RAND_MAX / 2;
 
 	if (n > 0)
-{
+
 	printf("%d is positive\n", n);
-}
+
 	else if (n < 0)
-{
+
 	printf("%d is negetive\n", n);
-}
+
 	else 
-{
+
 	printf("%d is zero\n", n);
-}	
+	
 	return (0);
 }
