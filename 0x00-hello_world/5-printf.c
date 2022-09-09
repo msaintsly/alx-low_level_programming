2c3edbc143a0d8b20425d30712145cd69134e64c
Musa Sylvester Echi <msaintsly@gmail.com>
2022-09-08 17:34:48 -0700

    task 5

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * main - C program that prints exactly with proper grammar, but the outcome is a piece of art
+ *
+ * Return: when 0 (it is successful)
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
