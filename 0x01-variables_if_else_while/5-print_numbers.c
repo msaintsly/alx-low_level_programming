fa89cdc3ea2725042590f9e819542f3cb6fceaa2
Musa Sylvester Echi <msaintsly@gmail.com>
2022-09-09 13:39:49 -0700

    task 5

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <time.h>
+#include <stdlib.h>
+
+/**
+ * main - Point of entry
+ *
+ * Return: always 0 (successfuk)
+ */
+
+int main(void)
+{
+	int i;
+	fot (i = 0; i < 10; i++)
+		printf ("%d", i)
+	printf("\n");
+
+	return (0);
+}
