0ef0d49ab94725f3dc453ab0791d225494e49b42
Musa Sylvester Echi <msaintsly@gmail.com>
2022-09-09 12:40:13 -0700

    task 3

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -8,8 +8,11 @@
 int main(void)
 {
 	char lower_case
-		for (lower_case = 'a'; lower_case <= 'z'; lower_case++)
+	for (lower_case = 'a'; lower_case <= 'z'; lower_case++)
 			putchar (lower_case);
+	for (lower_case = 'A'; LOWER_CASE <= 'Z'; lower_case++
+			putchar (lower_case);
+		
 	putchar ('\n');
 	return (0);
 }
