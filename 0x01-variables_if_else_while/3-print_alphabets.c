2e27c1318d2feead02628cfefd64be01b894e55c
Musa Sylvester Echi <msaintsly@gmail.com>
2022-09-09 07:01:20 -0700

    task 3

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - Alphabets that print in lowercase
+ *
+ * Return: Always 0 (successful)
+ */
+int main(void)
+{
+	char lower_case
+		for(lower_case = 'a'; lower_cae <= 'z'; lower_case++)
+			putchar (lower_case);
+	putchar ('\n');
+	return (0);
+}
