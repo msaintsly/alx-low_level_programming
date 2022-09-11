acaa5aa5e4c95da68b007844f69c978f30ed3d07
Musa Sylvester Echi <msaintsly@gmail.com>
2022-09-11 15:05:39 -0700

    task8

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -1,23 +1,28 @@
 #include <stdio.h>
 
 /**
- * main - Prints all the numbers of base 16 in lowercase
- *
- * Description: Using the main function
- * this program prints all the numbers of base 16 in lowercase
- * Return: Always 0 (sucessful)
- */
+* main - program that prints all possible combinations of single-digit numbers
+*
+* Description: Using mainf function
+* this prints all possible combinations of single-digit numbers
+* Return: Always 0 (successful)
+/
 int main(void)
 {
-	char c;
-	for (c  = '0'; <= '9'; c++)
+char c;
+for (c = '0'; c <= '9'; c++)
 {
-		putchar(c);
+	putchar(c);
 }
-	for (c = 's'; <= 'f'; c++)
+for (c = 'a'; c <= 'f'; c++)
 {
-		putchar(c);
+	putchar(c);
 }
-	putchar('\n');
-	return (0);
+putchar('\n');
+return (0);
 }
+^c
+
+[A
+[B
+[D[C
