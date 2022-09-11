602e01789ab377b4010a695f5d041a6641913757
Musa Sylvester Echi <msaintsly@gmail.com>
2022-09-11 14:33:16 -0700

    task 9

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - Printing all possible combinations of single-digit numbers
+ *
+ * Description: Using the main function
+ * this program prints "0-9"
+ * Return: Always 0 (successful)
+ */
+int main(void)
+{
+	int c;
+
+	for (c = '48'; c <= '57'; c++)
+		putcha(c);
+	if c != 57)
+		putchar(',');
+	putchar(' ');
+	putchar('\n');
+	return (0);
+}
