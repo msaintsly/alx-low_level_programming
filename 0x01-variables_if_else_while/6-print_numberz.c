cfd0c962bba3eeaaaa827a1d5c3e05a46a6870c0
Musa Sylvester Echi <msaintsly@gmail.com>
2022-09-12 01:56:02 -0700

    task 6

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -1,3 +1,21 @@
 #include <stdio.h>
 
+/**
+* main - prints the numbers 0-9
+*
+* Description: Using the main function
+* this program prints the numbers between 0-9
+* Return: Always 0 (Successful)
+*/
 
+int main(void)
+{
+	int c;
+	
+	for (c = '0'; c <= '9'; c++)
+	{
+		putchar(c);
+	}
+	putchar('\n');
+	return (0);
+}
