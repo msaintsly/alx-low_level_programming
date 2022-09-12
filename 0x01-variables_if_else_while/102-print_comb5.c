dc11bc60673939571f77de056a5a8f86d6c2dce8
Musa Sylvester Echi <msaintsly@gmail.com>
2022-09-12 16:20:30 -0700

    task 12

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -11,7 +11,7 @@ int main(void)
 {
 	int p, q;
 
-	for (p = 0; p <=98; p++)
+	for (p = 0; p <= 98; p++)
 	{
 		for (q = p + 1; q <= 99; q++)
 		{
@@ -21,7 +21,7 @@ int main(void)
 			putchar((q / 10) + '0');
 			putchar((q % 10) + '0');
 
-			if (p == 98 && q ==99)
+			if (p == 98 && q == 99)
 				continue;
 			putchar(',');
 			putchar(' ');
