d5342194b34d68feadd51cf9679229e6cf979f20
Musa Sylvester Echi <msaintsly@gmail.com>
2022-09-12 05:35:24 -0700

    task 12

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+* main - prints all possible combinations of two two-digit numbers
+*
+* Description: Using the main function
+* this program prints all possible combinations of two two-digit numbers
+* Return: 0 (successful)
+*/
+int main(void)
+{
+	int n1 = 0, n2;
+	while (n1 <= 99)
+	{
+		n2 = n1
+		while (n2 <= 99)
+		{
+			if (n2 != n1)
+			{
+				putchar ((n1 / 10) + 48);
+				putchar((n1 % 10) + 48);
+				putchar(' ');
+				putchar((n2 / 10) + 48);
+				putchar((n2 % 10) + 48);
+				if (n1 !=98 || n2 != 98)
+				{
+					putchar(',');
+					putchar(' ');
+				}
+			}
+			++n2;
+		}
+		++n1;
+	}
+	putchar('\n');
+	return (0);
+}
