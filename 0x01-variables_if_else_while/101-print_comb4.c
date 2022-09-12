32bfca3843f611720f102c575ff763a5a811d34b
Musa Sylvester Echi <msaintsly@gmail.com>
2022-09-12 04:03:03 -0700

    task 11

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,43 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+* main - prints all possible different combinations of three digits
+*
+* Description: Using the min function
+* this program prints all possible different combinations of three digits
+ Return: Always 0 (successful)
+*/
+int main (void)
+{
+	int n1 = 0, n2, n3;
+
+	while (n1 <= 9)
+	{
+		n2 = 0;
+		while (n2 <= 9)
+		{ 
+			n3 = 0;
+			while (n3 <= 9)
+			{
+				if (n1 != n2 && n1 < n2 && n2 != n3 && n2 < n3)
+				{
+					putchar (n1 + 48);
+					putchar (n2 + 48);
+					putchar(n3 + 48);
+					if (n1 + n2 + n3 != 24)
+					{
+						putchar(',');
+						putchar(' ');
+					}
+
+				}
+				++n3;
+			}
+			++n2;
+		}
+		++n1;
+	}
+	putchar('\n');
+	return (0);
+}
