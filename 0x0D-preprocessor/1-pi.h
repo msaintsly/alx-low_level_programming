8736f775d9795719f25f0b33682d15558833a58e
Musa Sylvester Echi <msaintsly@gmail.com>
2022-10-11 16:26:12 -0700

    all tasks

---
+++ 0x0D-preprocessor/1-pi.h
@@ -0,0 +1,3 @@
+#ifndef PI
+#define PI .14159265359
+#endif
--- 0x0D-preprocessor/HEADER.h
+++
@@ -1,6 +0,0 @@
-#ifndef _HEADER_
-#define _HEADER_
-#define SIZE 1024
-#define 0-OBJECT_LIKE_MACRO.H
-#ifndef 0-OBJECT_LIKE_MACRO.H
-#endif
