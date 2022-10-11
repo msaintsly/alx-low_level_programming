646d96cd2dc152e446337c1c6644bf1695e91c69
Musa Sylvester Echi <msaintsly@gmail.com>
2022-10-11 16:45:27 -0700

    tasks

--- 0x0D-preprocessor/2-main.c
+++ 0x0D-preprocessor/2-main.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
 /**
- * main - check the code
- *
+ * main - print the num of the file program use compiled from
  * Return: Always 0.
  */
+
 int main(void)
 {
-    printf("%s\n", _FILE_);
+    printf("%s\n", __FILE__);
     return (0);
 }
