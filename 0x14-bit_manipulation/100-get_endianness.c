4e7b41d62f53ed12fd047e34056f75f6f34e076b
Musa Sylvester Echi <msaintsly@gmail.com>
2022-10-27 15:20:19 -0700

    task adv

--- 0x14-bit_manipulation/100-get_endianness.c
+++ 0x14-bit_manipulation/100-get_endianness.c
@@ -1,2 +1,15 @@
-git add .
-git
+#include "main.h"
+
+/**
+ * get_endianness - checks the endianness
+ * Return: 0 if big endian, 1 if little endian
+ */
+int get_endianness(void)
+{
+	unsigned int x = 1;
+	char *c;
+
+	c = (char *)&x;
+
+	return ((int)*c);
+}
