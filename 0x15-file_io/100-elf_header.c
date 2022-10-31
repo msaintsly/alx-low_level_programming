306214c924edf981644f95dfe6a808bd3c1e4e4d
Musa Sylvester Echi <msaintsly@gmail.com>
2022-10-31 14:41:55 -0700

    task adv

--- 0x15-file_io/100-elf_header.c
+++ 0x15-file_io/100-elf_header.c
@@ -17,6 +17,7 @@ void print_abi(unsigned char *e_ident);
 void print_type(unsigned int e_type, unsigned char *e_ident);
 void print_entry(unsigned long int e_entry, unsigned char *e_ident);
 void close_file(int elf_file);
+int main(int __attrubute__((__unused)) argc, char *argv[]);
 
 /*
  * e_ident[] - IDENTIFICATION INDEXES
